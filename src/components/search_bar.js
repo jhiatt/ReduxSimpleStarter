c64938fc60ef169d7af6f0bdb42203b0a6ededb9
Jordan Hiatt <jordan.hiatt11@gmail.com>
2018-02-08 11:43:20 -0500

    wip

---
+++ src/components/search_bar.js
@@ -0,0 +1,41 @@
+// imput will triger API call
+import React, { Component } from 'react';
+// file now recognizes Component
+
+//class based component whenever you need a comp. to be able to be introspective
+//child class of React
+//render required, must return jsx as well.
+//class based components have "state", functional comp. do not
+class SearchBar extends Component {
+  //first fuction automatically called when new instance of class created
+  constructor(props) {
+    //super jumps to parent class, ie React.Component
+    super(props);
+
+    //this is where the value of the (input) variable is saved
+    //creating state variable in constructor?
+    this.state = { term: 'Search Here' };
+  }
+
+  render() {
+    // return <input onChange={this.onInputChange} />
+    //must use setState to inform react that state has changed, trigers background functionality
+    return (
+      <div>
+        <input 
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value})} />
+      </div>
+    ); 
+  }
+
+//common naming convention
+//event object created by the change (built in properties, target, value, etc)
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+}
+
+
+// tells the other files what to search for
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
+//core react library dealing with creating componants (incl jsx syntax)
+//libraries imported don't need a path
 import React from 'react';
+//library dealing with rendering the java scrip to the DOM
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// SearchBar is the function named in the search_bar file.
+import SearchBar from './components/search_bar'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+
+//const is an ES6 syntax.  Like variable but constant, will not change.
+  //we use const because the variable using the componant will not change (cannot change)
+//ES6 is the latest version of javascript (technically known as ECMAScript)
+
+//write the componant
+//this is a class not an instance (can have many instances of App)
+// const App = function() {
+  //same as above using new es6 syntax, also changes 'this' so that you can add multiple fuctions within a constant
+const App = () => {
+  //not html actually jsx (uses javascript)
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+//translated to vanila js
+// "use strict";
+// var _temporalUndefined = {};
+// var App = _temporalUndefined;
+
+// App = function App() {
+//   return React.createElement(
+//     "div",
+//     null,
+//     "Hi!"
+//   );
+// };
+
+
+//now we have to put the componant into the page
+//have access through react when we ran NPM install, stored in node_modules, connected through the first import line above.
+//This will create an instance of App, <App /> = <App></App> in the jsx
+//need to say where to put it, in this case div with class containor is our root REACT node
+ReactDOM.render(<App />, document.querySelector('.container'));
