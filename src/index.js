66aa8c308b5fea27367f2cdfe434da577ba593c1
Jordan Hiatt <jordan.hiatt11@gmail.com>
2018-02-12 10:50:19 -0500

    selecting videos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 //core react library dealing with creating componants (incl jsx syntax)
 //libraries imported don't need a path
-import React from 'react';
+import React, { Component } from 'react';
 //library dealing with rendering the java scrip to the DOM
 import ReactDOM from 'react-dom';
-
 // SearchBar is the function named in the search_bar file.
 import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
+// import API_KEY from 'key.js'
+
+const API_KEY = 'AIzaSyCDxHhXXv43QKieuFYblwVzqEOMvjqFCns';
 
 
 
@@ -18,13 +23,33 @@ import SearchBar from './components/search_bar'
 //this is a class not an instance (can have many instances of App)
 // const App = function() {
   //same as above using new es6 syntax, also changes 'this' so that you can add multiple fuctions within a constant
-const App = () => {
-  //not html actually jsx (uses javascript)
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+//downward date flow: the parent js file should fetch all the data and pass it down.
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      videos: [], 
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos });
+    });
+  }
+
+  render() {
+    //not html actually jsx (uses javascript)
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList videos={this.state.videos} />
+      </div>
+      //passed videos in as an array
+    );
+  }
 }
 //translated to vanila js
 // "use strict";
